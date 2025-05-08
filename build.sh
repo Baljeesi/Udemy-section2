ac33121baa5711b50b419367307c5a75936cd2cb
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-05-08 12:45:55 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-sudo docker build -t react-nginx .
-#create a docker hub account before pushing image to docker hub
-#make sure you did docker login with docker command "sudo docker login"
-sudo docker tag react-nginx sagarkakkalasworld/react-nginx
-sudo docker push sagarkakkalasworld/react-nginx
