787fa1ec4fd88e67ad50574868334a160919412c
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-05-08 13:33:32 +0530

    Update push_docker_image.sh

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-sudo docker build -t react-nginx .
+sudo docker build -t docker-deployment .
 #create a docker hub account before pushing image to docker hub
 #make sure you did docker login with docker command "sudo docker login"
-sudo docker tag react-nginx sagarkakkalasworld/react-nginx
-sudo docker push sagarkakkalasworld/react-nginx
+sudo docker tag react-nginx sagarkakkalasworld/docker-deployment
+sudo docker push sagarkakkalasworld/docker-deployment
